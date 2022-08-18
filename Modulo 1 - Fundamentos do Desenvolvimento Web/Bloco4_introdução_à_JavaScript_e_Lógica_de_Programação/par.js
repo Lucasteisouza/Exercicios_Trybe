608c36b774319545b474ee55bf3f1f074c987533
Lucasteisouza <lucasteisouza@gmail.com>
2022-08-18 16:14:27 -0300

    Primeiros 8 exercicios

---
+++ Modulo 1 - Fundamentos do Desenvolvimento Web/Bloco4_introdução_à_JavaScript_e_Lógica_de_Programação/par.js
@@ -0,0 +1,10 @@
+const primeiroNum = 19;
+const segundoNum = 15;
+const terceiroNum = 3;
+
+if (primeiroNum % 2 === 0 || segundoNum % 2 === 0 || terceiroNum % 2 === 0){
+    console.log (true)
+}
+else{
+    console.log (false)
+}
---
+++ Modulo 1 - Fundamentos do Desenvolvimento Web/Bloco4_introdução_à_JavaScript_e_Lógica_de_Programação/primeiro.js
@@ -0,0 +1,9 @@
+const myName = 31;
+const birthCity = 'BH';
+let birthYear = 1990;
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
+birthYear = 2030
+console.log(birthYear);
+birthCity = 'outroLugar';
