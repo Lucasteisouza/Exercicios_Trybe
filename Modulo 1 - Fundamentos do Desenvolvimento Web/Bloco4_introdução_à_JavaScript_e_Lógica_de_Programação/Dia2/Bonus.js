8b67333f75df21f62d44b15418ef208df0a05fc3
Lucasteisouza <lucasteisouza@gmail.com>
2022-08-19 19:00:53 -0300

    exercicio bonus

---
+++ Modulo 1 - Fundamentos do Desenvolvimento Web/Bloco4_introdução_à_JavaScript_e_Lógica_de_Programação/Dia2/Bonus.js
@@ -0,0 +1,15 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let aux;
+let orderedNumbers =[];
+
+for(let index = 1; index < numbers.length; index += 1){
+    for (let index2 = 0; index2 < index; index2 += 1){
+        if(numbers[index] < numbers[index2]){
+        aux = numbers[index];
+        numbers[index]= numbers[index2];
+        numbers[index2] = aux; 
+        }
+    }
+}
+
+console.log(numbers);
