319462c23a218d52c3915ba8984377aa3085d54a
Lucasteisouza <lucasteisouza@gmail.com>
2022-08-19 15:26:36 -0300

    foguete

--- Modulo 1 - Fundamentos do Desenvolvimento Web/Bloco4_introdução_à_JavaScript_e_Lógica_de_Programação/Dia2/foguete.js
+++ Modulo 1 - Fundamentos do Desenvolvimento Web/Bloco4_introdução_à_JavaScript_e_Lógica_de_Programação/Dia2/foguete.js
@@ -1,5 +1,8 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let numAtual=0;
 
-for (let i = 0; i < numbers.length; i=+ 1){
-    console.log(numbers[i]);
+for (let i = 0; i < numbers.length; i += 1){
+    if (numbers[i] > numAtual){
+        numAtual = numbers[i];
+    }
 }
