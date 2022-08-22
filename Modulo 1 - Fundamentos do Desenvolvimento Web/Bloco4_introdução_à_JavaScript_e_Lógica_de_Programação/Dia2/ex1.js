978ed83a7270692bc7ba1bd3dedf706042bae27a
Lucasteisouza <lucasteisouza@gmail.com>
2022-08-22 17:05:35 -0300

    atualiza exercicios do bloco 4.2

--- Modulo 1 - Fundamentos do Desenvolvimento Web/Bloco4_introdução_à_JavaScript_e_Lógica_de_Programação/Dia2/ex1.js
+++ Modulo 1 - Fundamentos do Desenvolvimento Web/Bloco4_introdução_à_JavaScript_e_Lógica_de_Programação/Dia2/ex1.js
@@ -1,13 +1,13 @@
-let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let menu = ["Home", "Serviços", "Portfólio", "Links"];
 let menuServices = menu[1];
 
 console.log(menuServices);
 
-menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
-let indexOfPortfolio = menu.indexOf('Portfólio');
+menu = ["Home", "Serviços", "Portfólio", "Links"];
+let indexOfPortfolio = menu.indexOf("Portfólio");
 
 console.log(indexOfPortfolio);
 
-menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
-menu.push('Contato');
+menu = ["Home", "Serviços", "Portfólio", "Links"];
+menu.push("Contato");
 console.log(menu);
--- Modulo 1 - Fundamentos do Desenvolvimento Web/Bloco4_introdução_à_JavaScript_e_Lógica_de_Programação/Dia2/ex3.js
+++ Modulo 1 - Fundamentos do Desenvolvimento Web/Bloco4_introdução_à_JavaScript_e_Lógica_de_Programação/Dia2/ex3.js
@@ -1,4 +1,4 @@
-let names = ['João', 'Maria', 'Antônio', 'Margarida'];
-for (nome of names){
+let names = ["João", "Maria", "Antônio", "Margarida"];
+for (nome of names) {
     console.log(nome);
 }
