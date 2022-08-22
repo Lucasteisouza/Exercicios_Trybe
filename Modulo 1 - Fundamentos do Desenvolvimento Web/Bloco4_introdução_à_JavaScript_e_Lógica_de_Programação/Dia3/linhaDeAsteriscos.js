305d9344f53ff1969cce341f5997a092dd4c7aa9
Lucasteisouza <lucasteisouza@gmail.com>
2022-08-22 19:33:25 -0300

    exercicios do asterisco

--- Modulo 1 - Fundamentos do Desenvolvimento Web/Bloco4_introdução_à_JavaScript_e_Lógica_de_Programação/Dia3/linhaDeAsteriscos.js
+++ Modulo 1 - Fundamentos do Desenvolvimento Web/Bloco4_introdução_à_JavaScript_e_Lógica_de_Programação/Dia3/linhaDeAsteriscos.js
@@ -1,6 +1,78 @@
-let n = 6;
-let linhaDeAsteriscos = "";
-for (i = 1; i < n ; i += 1) {
-    linhaDeAsteriscos = linhaDeAsteriscos + " ";
-    console.log(linhaDeAsteriscos + '*');
+// let n = 6;
+
+// for(j = 1; j < n; j += 1){
+//     let linhaDeAsteriscos = "";
+//     for (i = 1; i < n ; i += 1) {
+//         linhaDeAsteriscos = linhaDeAsteriscos + "*";
+//     }
+//     console.log(linhaDeAsteriscos);
+// }
+
+
+// let n = 6;
+// let linhaDeAsteriscos = "";
+// for (i = 1; i < n ; i += 1) {
+//     linhaDeAsteriscos = linhaDeAsteriscos + "*";
+//     console.log(linhaDeAsteriscos);
+// }
+
+// let n = 6;
+// let arrayDeAsteriscos = []
+// for(index = 0; index < n; index += 1){
+//     arrayDeAsteriscos.push(' ')
+// }
+// for (index  = 0; index < n; index +=1){
+//     arrayDeAsteriscos.shift();
+//     arrayDeAsteriscos.push('*')
+//     let linhaDeAsteriscos= '';
+//     for (index2 = 0; index2 < n; index2 += 1){
+//         linhaDeAsteriscos = linhaDeAsteriscos + arrayDeAsteriscos[index2]
+//     }
+//     console.log(linhaDeAsteriscos)
+    
+// }
+
+// let n = 7;
+// let arrayDeAsteriscos = []
+// for(index = 0; index < n; index += 1){
+//     arrayDeAsteriscos.push(' ')
+// }
+// for (index  = 0; index < n; index +=1){
+//     arrayDeAsteriscos.shift();
+//     arrayDeAsteriscos.unshift('*');
+//     arrayDeAsteriscos.pop();
+//     arrayDeAsteriscos.push('*');
+//     console.log(arrayDeAsteriscos)
+// }
+
+
+    // let linhaDeAsteriscos= '';
+    // for (index2 = 0; index2 < n; index2 += 1){
+    //     linhaDeAsteriscos = linhaDeAsteriscos + arrayDeAsteriscos[index2]
+    // }
+    // console.log(linhaDeAsteriscos)
+    
+let n = 7;
+let arrayCentral = ['*'];
+let arrayLeft =[];
+let arrayRight = [];
+for(index =1; index< n/2; index += 1){
+    arrayLeft.unshift(' ');
+    arrayRight.push(' ')
 }
+console.log(arrayLeft, arrayCentral, arrayRight)
+
+for (index = 1; index < n/2; index += 1){
+    arrayLeft.shift();
+    arrayLeft.push('*');
+    arrayRight.pop();
+    arrayRight.unshift('*')
+    let linhaDeAsteriscos ='';
+    for (index2 =0; index2 <n/2; index2 += 1){
+        linhaDeAsteriscos = arrayLeft[index2] + arrayCentral + arrayRight[index2]
+    }
+    console.log(linhaDeAsteriscos)
+}
+
+
+
